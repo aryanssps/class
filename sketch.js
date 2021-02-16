0a64e9d02f8cf3b173fad1344176088c2c4b981b
aryanssps <74345307+aryanssps@users.noreply.github.com>
2021-02-16 13:00:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+
+var engine;
+var world;
+var ground;
+
+function Setup(){
+  createCanvas(1200,650)
+
+ 
+  engine = Engine.create();
+  world = engine.world;
+
+ ground=new Ground(600,640,1200,10)
+
+}
+
+function draw(){
+  background(255)
+
+  Engine.update(engine)
+
+  ground.display();
+  drawSprites();
+}
